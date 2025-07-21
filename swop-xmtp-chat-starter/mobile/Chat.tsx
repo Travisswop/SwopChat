04d51c62b813ed6c1de047ab1a4c324004a24a7a
Swop Technologies INC <98964561+Travisswop@users.noreply.github.com>
2025-07-20 20:38:35 -0400

    Update Chat.tsx

--- swop-xmtp-chat-starter/mobile/Chat.tsx
+++ swop-xmtp-chat-starter/mobile/Chat.tsx
@@ -1,1 +1,21 @@
+import React, { useEffect } from 'react';
+import { View, Text } from 'react-native';
+import { initXMTP, getClient } from '../shared/xmtpClient';
+
+export default function Chat() {
+  useEffect(() => {
+    const load = async () => {
+      const client = await initXMTP(userAddress, privyProvider);
+      const convos = await client.conversations.list();
+      console.log(convos);
+    };
+    load();
+  }, []);
+
+  return (
+    <View>
+      <Text>Mobile Chat UI Here</Text>
+    </View>
+  );
+}
 // React Native chat UI wrapper
